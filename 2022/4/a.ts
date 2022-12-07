7bce303621f1637b627eb3ca1f6609307d7bbc6b
Michal Bastrzyk <michal.bastrzyk.pl@gmail.com>
2022-12-07 17:40:26 +0100

    On a strike 😎

---
+++ 2022/4/a.ts
@@ -0,0 +1,29 @@
+export const x = "";
+
+const input = await Deno.readTextFile("./input.txt");
+
+const lines: string[] = input.split("\n");
+
+let score: number = 0;
+
+for (const line in lines) {
+  const element = lines[line];
+
+  const pairs: string[] = element.split(",");
+  const firstRange: number[] = pairs[0].split("-").map((e) => parseInt(e));
+  const secondRange: number[] = pairs[1].split("-").map((e) => parseInt(e));
+
+  let firstNumbers: number[] = [];
+  let secondNumbers: number[] = [];
+
+  for (let i = firstRange[0]; i <= firstRange[1]; i++) firstNumbers.push(i);
+  for (let i = secondRange[0]; i <= secondRange[1]; i++) secondNumbers.push(i);
+
+  const contains =
+    firstNumbers.every((n) => secondNumbers.includes(n)) ||
+    secondNumbers.every((n) => firstNumbers.includes(n));
+
+  if (contains) score += 1;
+}
+
+console.log(score);
