9128b40fea66ed66859acc5f9b5bc5590b7fcbbe
Michal Bastrzyk <michal.bastrzyk.pl@gmail.com>
2022-12-18 17:33:10 +0100

    Ez 👹

---
+++ 2022/6/a.ts
@@ -0,0 +1,26 @@
+export const x = "";
+
+const input = await Deno.readTextFile("./input.txt");
+
+const lines: string[] = input.split("\n");
+
+const scores: number[] = [];
+
+for (let i = 0; i < lines.length; i++) {
+    const element = lines[i];
+    const elementSplit = element.split("");
+
+    for (let j = 0; j < elementSplit.length; j++) {
+        const slice = [...elementSplit].slice(j, j + 4);
+
+        const filtered = slice.filter((v, i) => slice.indexOf(v) !== i);
+        if (filtered.length === 0) { 
+            
+            scores.push(j + 4);
+            break;
+        }
+    }
+}
+
+console.log(scores);
+
